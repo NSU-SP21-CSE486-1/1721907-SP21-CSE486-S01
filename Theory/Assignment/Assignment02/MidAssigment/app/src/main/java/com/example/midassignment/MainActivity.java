0bf0556b8f7794e371f1ec40ae0e9da35f6914aa
Ahnaf Ahmad <ahnafahmad95@gmail.com>
2021-04-09 20:19:22 +0600

    Dependency of School & Department #40
    Dependency of School & Department code implemented in Android Studio

--- Theory/Assignment/Assignment02/MidAssigment/app/src/main/java/com/example/midassignment/MainActivity.java
+++ Theory/Assignment/Assignment02/MidAssigment/app/src/main/java/com/example/midassignment/MainActivity.java
@@ -25,15 +25,44 @@ public class MainActivity extends AppCompatActivity  {
 
 
         schoolList = findViewById(R.id.School_list_Id);
+        deptList = findViewById(R.id.dept_List_Id);
 
         String[] schoolListString = getResources().getStringArray(R.array.school_list);
-
+        String[] eceListString = getResources().getStringArray(R.array.ece_dept_list);
+        String[] sbeListString = getResources().getStringArray(R.array.sbe_dept_list);
+        String[] shssListString = getResources().getStringArray(R.array.shss_dept_list);
+        String[] shlsListString = getResources().getStringArray(R.array.shls_dept_list);
 
         ArrayAdapter schoolListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, schoolListString);
+        ArrayAdapter ecelListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, eceListString);
+        ArrayAdapter sbeListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sbeListString);
+        ArrayAdapter shssListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, shssListString);
+        ArrayAdapter shlsListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, shlsListString);
 
 
         schoolList.setAdapter(schoolListAdapter);
+        schoolList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                if (position == 0) {
+                    deptList.setAdapter(ecelListAdapter);
+                }
+
+                else if (position == 1) {
+                    deptList.setAdapter(sbeListAdapter);
+                }
+
+                else if (position == 2) {
+                    deptList.setAdapter(shssListAdapter);
+                }
+
+                else if (position == 3) {
+                    deptList.setAdapter(shlsListAdapter);
+                }
+
+            }
+        });
 
     }
 
