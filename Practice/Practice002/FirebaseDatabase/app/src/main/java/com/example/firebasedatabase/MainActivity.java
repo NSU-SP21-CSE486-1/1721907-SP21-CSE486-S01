4ba1353a7f5bed1801fce1f81057cd7c4d00ca50
Ahnaf Ahmad <ahnafahmad95@gmail.com>
2021-04-27 21:19:21 +0600

    how to store data in Firebase Database  60
    Completed how to store data in Firebase Database

---
+++ Practice/Practice002/FirebaseDatabase/app/src/main/java/com/example/firebasedatabase/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.firebasedatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView info;
+    private EditText name, age;
+    private Button submit;
+
+    DatabaseReference databaseReference;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        info = findViewById(R.id.infoId);
+        name = findViewById(R.id.nameId);
+        age = findViewById(R.id.ageId);
+        submit = findViewById(R.id.submitButtonId);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("students");
+    }
+
+    public void submit(View view) {
+
+        saveData();
+
+    }
+
+    private void saveData() {
+
+    String mName = name.getText().toString().trim();
+    String mAge = age.getText().toString().trim();
+
+    String key = databaseReference.push().getKey();
+
+    Student student = new Student(mName, mAge);
+    databaseReference.child(key).setValue(student);
+
+        Toast.makeText(getApplicationContext(), "Student Info is added", Toast.LENGTH_SHORT).show();
+
+    }
+}
