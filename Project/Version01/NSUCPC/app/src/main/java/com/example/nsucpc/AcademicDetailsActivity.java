542d6e4b9696ad735b8bac73b4ac265bf78f2755
Ahnaf Ahmad <ahnafahmad95@gmail.com>
2021-05-19 22:03:11 +0600

    Academic Details Screen Design for student  #75
    Completed Academic Details Screen for student

---
+++ Project/Version01/NSUCPC/app/src/main/java/com/example/nsucpc/AcademicDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.nsucpc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcademicDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_academic_details);
+    }
+}
