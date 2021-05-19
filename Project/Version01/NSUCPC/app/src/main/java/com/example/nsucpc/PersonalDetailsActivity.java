f2c00b89e62af26bfe4f3db168457e1b4bc30c90
Ahnaf Ahmad <ahnafahmad95@gmail.com>
2021-05-19 21:38:04 +0600

    personal Details Screen Design for student  #74
    Completed personal Details Screen Design for stduent

---
+++ Project/Version01/NSUCPC/app/src/main/java/com/example/nsucpc/PersonalDetailsActivity.java
@@ -0,0 +1,18 @@
+package com.example.nsucpc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class PersonalDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_details);
+    }
+
+    public void save(View view) {
+    }
+}
