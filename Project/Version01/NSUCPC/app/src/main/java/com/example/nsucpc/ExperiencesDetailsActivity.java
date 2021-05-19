4f4ff0595feac82e6c552a9bd8b4752f77369c3a
Ahnaf Ahmad <ahnafahmad95@gmail.com>
2021-05-20 03:29:31 +0600

    Experience Screen Design for Student  #78
    Completed Experience Screen Design for student

---
+++ Project/Version01/NSUCPC/app/src/main/java/com/example/nsucpc/ExperiencesDetailsActivity.java
@@ -0,0 +1,18 @@
+package com.example.nsucpc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ExperiencesDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_experiences_details);
+    }
+
+    public void save(View view) {
+    }
+}
