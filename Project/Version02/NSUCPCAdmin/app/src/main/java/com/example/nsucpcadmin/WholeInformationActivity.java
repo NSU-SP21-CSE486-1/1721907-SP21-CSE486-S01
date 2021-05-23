73b6e6ab933f18a6210a9c33d6bdd5b062cb3658
Ahnaf Ahmad <ahnafahmad95@gmail.com>
2021-05-23 17:54:20 +0600

    whole info page design   #92
    Completed the whole info page design

---
+++ Project/Version02/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/WholeInformationActivity.java
@@ -0,0 +1,17 @@
+package com.example.nsucpcadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class WholeInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whole_information);
+    }
+
+
+}
