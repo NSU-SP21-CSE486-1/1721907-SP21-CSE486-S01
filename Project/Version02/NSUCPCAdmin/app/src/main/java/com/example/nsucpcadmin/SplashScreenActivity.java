ae5bca69edd046587392180b4358ff4f14727586
Ahnaf Ahmad <ahnafahmad95@gmail.com>
2021-05-26 21:45:12 +0600

    Splash Screen for NSU CPC Admin App  #100
    Completed Splash Screen will be completed for NSU CPC Admin App

---
+++ Project/Version02/NSUCPCAdmin/app/src/main/java/com/example/nsucpcadmin/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package com.example.nsucpcadmin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private Button signUp, skip;
+    private TextView nsuCpc, showYourTalent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        signUp = findViewById(R.id.signUpId);
+        nsuCpc = findViewById(R.id.nsuCpcId);
+
+    }
+
+    public void signUp(View view) {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void skip(View view) {
+
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+
+    }
+}
